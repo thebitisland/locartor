d398cfa58934699a4ad1fd8e08766929449a79ed
Juan Luis Sanz <juanljr2@gmail.com>
2014-04-05 18:06:49 +0200

    Camera working

--- src/com/thebitisland/locartor/MainActivity.java
+++ src/com/thebitisland/locartor/MainActivity.java
@@ -2,14 +2,42 @@ package com.thebitisland.locartor;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+	ImageView imgFavorite;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		imgFavorite = (ImageView) findViewById(R.id.imageView1);
+		imgFavorite.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				open();
+			}
+		});
+	}
+
+	public void open() {
+		Intent intent = new Intent(
+				android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		startActivityForResult(intent, 0);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+		Bitmap bp = (Bitmap) data.getExtras().get("data");
+		imgFavorite.setImageBitmap(bp);
 	}
 
 	@Override
@@ -18,5 +46,4 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
 }
